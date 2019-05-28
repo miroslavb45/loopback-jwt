c3196f888990cb7bbafaf017f93f1c9b8050be6a
Brnic Miroslav <miroslav.brnic@webvalto.hu>
2019-05-28 18:05:27 +0200

    Authentication improvements

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
-import {AppApplication} from './application';
-import {ApplicationConfig} from '@loopback/core';
+import { AppApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
 
-export {AppApplication};
+export { AppApplication };
 
 export async function main(options: ApplicationConfig = {}) {
-  const app = new AppApplication(options);
+  const app = new AppApplication({
+    rest: {
+      port: 3000,
+      cors: {
+        origin: 'http://localhost:4200',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        maxAge: 86400,
+        credentials: true,
+
+      }
+    },
+
+
+  });
   await app.boot();
   await app.start();
 
   const url = app.restServer.url;
   console.log(`Server is running at ${url}`);
-  console.log(`Try ${url}/ping`);
+  // console.log(`Try ${url}/ping`);
 
   return app;
 }
