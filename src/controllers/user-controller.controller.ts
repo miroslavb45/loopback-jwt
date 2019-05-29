61473468f297300d6f277b74ed3dbb4c352a0ded
Brnic Miroslav <miroslav.brnic@webvalto.hu>
2019-05-29 15:51:47 +0200

    Auth fixes

--- src/controllers/user-controller.controller.ts
+++ src/controllers/user-controller.controller.ts
@@ -7,6 +7,7 @@ import { promisify } from 'util';
 
 const { sign } = require('jsonwebtoken');
 const signAsync = promisify(sign);
+const bcrypt = require('bcrypt');
 
 export class UserController {
   constructor(
@@ -26,38 +27,48 @@ export class UserController {
     const user = await this.userRepository.findOne({ where: { username: credentials.username } }) || await this.userRepository.findOne({ where: { email: credentials.username } });
 
     if (!user) throw new HttpErrors.Unauthorized('Invalid credentials');
+    const isPasswordMatched = bcrypt.compareSync(credentials.password, user.password); // true
 
-    const isPasswordMatched = user.password === credentials.password;
     if (!isPasswordMatched) throw new HttpErrors.Unauthorized('Invalid credentials');
 
     const tokenObject = credentials.username === undefined ? { email: credentials.email } : { username: credentials.username };
     const token = await signAsync(tokenObject, JWT_SECRET, { expiresIn: 60 * 60 * 24 * 3 }); //3 day expire
     const roles = await this.userRoleRepository.find({ where: { userId: user.id } });
-    const { id, email } = user;
+    const { id, email, username } = user;
 
     return {
       token,
       id: id as string,
       email,
-      roles: roles.map(r => r.roleId),
-      userId: id as string
+      username,
+      roles: roles.map(r => r.roleId)
     };
   }
 
-  @post('/register')
+  @post('/register', {
+    responses: {
+      '200': {
+        description: 'Successful registration',
+      },
+    },
+  })
   async register(@requestBody() credentials: Credentials) {
     const user = await this.userRepository.findOne({ where: { username: credentials.username } });
 
 
     if (user === null) {
-      let newUser: User = new User({ username: credentials.username, password: credentials.password });
+      var hashedPassword = bcrypt.hashSync(credentials.password, 10);
+
+      let newUser: User = new User({ username: credentials.username, password: hashedPassword });
       newUser = await this.userRepository.create(newUser);
       let newUserRole: UserRole = new UserRole({ userId: newUser.id, roleId: "USER" });
       this.userRoleRepository.create(newUserRole);
-      this.login(credentials);
+
+
+
 
     } else {
-      throw new HttpErrors.NotAcceptable('User already exists');
+      throw new HttpErrors.NotAcceptable('User already exists!');
     }
 
   }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import {AppApplication} from './application';
-import {ApplicationConfig} from '@loopback/core';
+import { AppApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
 import * as fs from 'fs';
 
 
@@ -10,21 +10,20 @@ export async function main(options: ApplicationConfig = {}) {
 
   const app = new AppApplication({
 
-	rest: {
-	port: 3001,
- 	protocol: 'https',
-     	key: fs.readFileSync('/etc/letsencrypt/live/devdevdev.tk/privkey.pem'),
-      	cert: fs.readFileSync('/etc/letsencrypt/live/devdevdev.tk/cert.pem'),   
- 		cors: {
-          origin: process.env.ORIGIN,
-          methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-          preflightContinue: false,
-          optionsSuccessStatus: 204,
-          maxAge: 86400,
-          credentials: true,
+    rest: {
+      port: process.env.PORT,
 
-        	} 
-	}  });
+      cors: {
+        origin: process.env.ORIGIN,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        maxAge: 86400,
+        credentials: true,
+
+      }
+    }
+  });
 
   await app.boot();
   await app.start();
