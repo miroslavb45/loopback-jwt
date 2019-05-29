ffa4f9f86fb2cbbe42ac730aa9186cb5ef96250a
root <miroslavb45@gmail.com>
2019-05-29 11:38:22 +0200

    Local

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
-import { AppApplication } from './application';
-import { ApplicationConfig } from '@loopback/core';
+import {AppApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+import * as fs from 'fs';
+
+
 
 export { AppApplication };
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new AppApplication({
-    rest: {
-      port: 3001,
-      cors: {
-        origin: 'https://devdevdev.tk',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-        maxAge: 86400,
-        credentials: true,
 
-      }
-    },
+	rest: {
+	port: 3001,
+ 	protocol: 'https',
+     	key: fs.readFileSync('/etc/letsencrypt/live/devdevdev.tk/privkey.pem'),
+      	cert: fs.readFileSync('/etc/letsencrypt/live/devdevdev.tk/cert.pem'),   
+ 		cors: {
+        		origin: 'https://devdevdev.tk',
+        		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        		preflightContinue: false,
+        		optionsSuccessStatus: 204,
+        		maxAge: 86400,
+        		credentials: true,
 
-
-  });
+      			}     
+	}  });
   await app.boot();
   await app.start();
 
